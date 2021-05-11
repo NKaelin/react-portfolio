21fec447102d4e08836531190920f269dce92c25
NKaelin <75902526+NKaelin@users.noreply.github.com>
2021-05-10 22:36:37 -0400

    portfolio function added

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,12 +3,26 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+
+        console.log('Page rendered');
+    }
+
+    portfolioItems() {
+        const data = ['Cali', 'Hawaii', 'Florida'];
+
+        return data.map(item => {
+            return <PortfolioItem />;
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Portfolio items go here updated...</h2>
 
-                <PortfolioItem />
+                {this.portfolioItems()}
             </div>
         );
     }
