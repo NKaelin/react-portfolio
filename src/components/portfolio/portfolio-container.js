690e6acde40383caad3fb3904e24b4e12459914e
NKaelin <75902526+NKaelin@users.noreply.github.com>
2021-05-18 11:29:55 -0400

    added filter

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,14 +8,24 @@ export default class PortfolioContainer extends Component {
 
         this.state = {
             pageTitle: "Welcome to my portfolio!",
+            isLoading: false,
             data: [
-                {title: "California"},
-                {title: "Hawaii"},
-                {title: "Florida"}
+                {title: "California", category: "West Coast"},
+                {title: "Thailand", category: "Asia"},
+                {title: "Hawaii", category: "Pacific Ocean"},
+                {title: "Florida", category: "East Coast"}
             ]
         };
 
-        this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
+        this.handleFilter = this.handleFilter.bind(this);
+    }
+
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        });
     }
 
     portfolioItems() {
@@ -31,15 +41,21 @@ export default class PortfolioContainer extends Component {
     }
 
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>;
+        }
+
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
 
+                <button onClick={() => this.handleFilter('West Coast')}>West Coast</button>
+                <button onClick={() => this.handleFilter('Asia')}>Asia</button>
+                <button onClick={() => this.handleFilter('Pacific Ocean')}>Pacific Ocean</button>
+                <button onClick={() => this.handleFilter('East Coast')}>East Coast</button>
+
                 {this.portfolioItems()}
 
-                <hr/>
-
-                <button onClick={this.handlePageTitleUpdate}>Change title</button>
             </div>
         );
     }
