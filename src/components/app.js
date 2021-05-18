29de628736ca86446c5f9a26b8863271d701d7f5
NKaelin <75902526+NKaelin@users.noreply.github.com>
2021-05-18 11:51:13 -0400

    added navigation component

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,13 @@ import moment from 'moment';
 
 
 import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>Natalie Kaelin Portfolio</h1>
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
